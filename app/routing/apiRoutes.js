b07a37209f72bc31013f25448351c384cba03c35
heplayskeys <heplayskeys@gmail.com>
2019-01-10 19:11:11 -0800

    Completed HW

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,10 +8,60 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", (req, res) => {
-        var newProfile = req.body;
-        
-        profiles.push(newProfile);
 
-        res.status(200).end();
+        var newProfile = req.body;
+        var profileTotals = [];
+        var sumTotals = [];
+        var matched;
+
+        profileTotal();
+
+        function profileTotal() {
+            for (let j = 0; j < profiles.length; j++) {
+
+                var compareTotals = [];
+
+                for (let i = 0; i < 10; i++) {
+                    if (profiles[j].scores[i] > newProfile.scores[i] || profiles[j].scores[i] === newProfile.scores[i]) {
+                        compareTotals.push(profiles[j].scores[i] - newProfile.scores[i]);
+                    }
+                    else {
+                        compareTotals.push(newProfile.scores[i] - profiles[j].scores[i]);
+                    }
+                }
+
+                console.log("HERE");
+                profileTotals.push(compareTotals);
+                sumTotal();
+            }
+        }
+
+        function sumTotal() {
+
+            for (let i = 0; i < profileTotals.length; i++) {
+                sumTotals[i] = profileTotals[i].reduce((a, b) => a + b, 0);
+            }
+
+            match();
+        }
+
+        function match() {
+
+            matched = sumTotals[0];
+
+            for (let i = 0; i < sumTotals.length; i++) {
+
+                if (sumTotals[i] > matched) {
+                    matched = sumTotals[i];
+                }
+            }
+
+            matched = sumTotals.indexOf(matched);
+        }
+
+        console.log(sumTotals);
+        console.log(matched);
+        profiles.push(newProfile);
+        return res.json(profiles[matched]);
     });
 };
